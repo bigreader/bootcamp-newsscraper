706ae53ff4c7388a06c0c5bee711277dec1fd60f
Joshua <bigreaderjd1996@gmail.com>
2019-06-04 14:57:21 -0500

    scrape and display list

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,4 @@
-var axios   = require('axios');
-var cheerio = require('cheerio');
 var express = require('express');
-var moment  = require('moment');
 var db      = require('../models');
 
 var router = express.Router();
@@ -22,20 +19,45 @@ router.get('/articles', function (req, res) {
   }).exec(handle(res));
 });
 
-
-router.get('/comments', function (req, res) {
-  db.Comment.find(handle(res));
+router.get('/articles/:article', function (req, res) {
+  db.Article.findById(req.params.article).populate('comments').exec((err, article) => {
+    if (err) return res.status(500).json({ error: err });
+    if (!article) return res.status(404).end();
+    res.json(article);
+  });
 });
 
-router.post('/comments', function (req, res) {
+
+router.get('/articles/:article/comments', function (req, res) {
+  db.Article.findById(req.params.article).populate('comments').exec((err, article) => {
+    if (err) return res.status(500).json({ error: err });
+    if (!article) return res.status(404).end();
+    res.json(article.comments);
+  });
+});
+
+router.post('/articles/:article/comments', function (req, res) {
   db.Comment.create(req.body, handle(res, 201));
+
+  db.Article.findById(req.params.article, (err, article) => {
+    if (err) return res.status(500).json({error: err});
+    if (!article) return res.status(404).end();
+
+    var comment = new db.Comment(req.body);
+    comment.save((err, data) => {
+      res.status(201).json(comment);
+    });
+    article.comments.push(comment._id);
+    article.save();
+  });
+
 });
 
-router.put('/comments/:id', function (req, res) {
+router.put('/articles/:article/comments/:comment', function (req, res) {
   db.Comment.updateOne({ _id: req.params.id }, req.body, handle(res));
 });
 
-router.delete('/comments/:id', function (req, res) {
+router.delete('/articles/:article/comments/:comment', function (req, res) {
   db.Comment.deleteOne({ _id: req.params.id }, handle(res));
 });
 
--- routes/main.js
+++ routes/main.js
@@ -1,36 +1,63 @@
+var axios   = require('axios');
+var cheerio = require('cheerio');
 var express = require('express');
-var db = require('../models');
+var moment  = require('moment');
+var db      = require('../models');
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  var $ = cheerio.load(page.data);
+  axios.get('https://www.macrumors.com/').then(page => {
+    var $ = cheerio.load(page.data);
 
-  $('div.article').each((i, div) => {
-    var article = new Article();
+    $('.article').each(function() {
+      var doc = {};
 
-    var title = div.find('.title a');
-    article.title = title.text();
-    article.link = title.attr('href');
+      var title = $('.title a', this);
+      doc.title = title.text();
+      doc.link = title.attr('href');
 
-    var byline = div.find('.byline');
-    article.author = byline.find('a').text();
-    var dateString = byline.text().split(' by')[0];
-    article.date = moment(dateString.replace(/P.T/, '-7:00'), 'dddd MMMM D, YYYY h:mm a Z');
+      var byline = $('.byline', this);
+      doc.author = byline.find('a').text();
+      var dateString = byline.text().split(' by')[0];
+      doc.date = moment(dateString.replace(/P.T/, '-7:00'), 'dddd MMMM D, YYYY h:mm a Z');
 
-    var content = div.find('.content .content_inner');
-    article.body = content.text();
-    article.img = content.find('noscript img').attr('src');
+      var content = $('.content .content_inner', this);
+      doc.body = content.text();
+      doc.img = content.find('noscript img').attr('src');
 
-    await article.save();
-  });
+      db.Article.updateOne({
+        link: $('.title a', this).attr('href')
+      }, {
+        title: $('.title a', this).text(),
+        link: $('.title a', this).attr('href'),
+        author: $('.byline a', this).text(),
+        body: $('.content_inner', this).text(),
+        img: $('.content noscript img').first().attr('src')
+      }, {
+        upsert: true
+      }).exec();
+    });
 
-  db.Article.find().sort({
-    date: -1
-  }).exec((err, data) => {
-    if (err) return res.status(500).send(err);
-    res.render('articles', data);
+    db.Article.find().sort({
+      date: -1
+    }).exec((err, data) => {
+      if (err) return res.status(500).send(err);
+      res.render('articles', {
+        articles: data
+      });
+    });
   });
 });
+
+
+router.get('/:id', (req, res) => {
+  db.Article.findById(req.params.id).populate('comments').exec((err, article) => {
+    if (err) return res.status(500).send(err);
+    if (!article) return res.status(404).end();
+    res.render('article', article);
+  });
+});
+
 
 module.exports = router;
